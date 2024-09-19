e5ed53a3e6d955e53505639eff8bcd0082f6cb92
multiverse22 <momizi527@gmail.com>
2024-09-19 17:25:46 +0900

    init commit

---
+++ src/test/java/com/sparta/nationofdevelopment/NationOfDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nationofdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationOfDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
