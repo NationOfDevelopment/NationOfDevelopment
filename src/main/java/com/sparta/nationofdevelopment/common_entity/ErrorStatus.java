59814e2d4973c69473ff6128880c39bf5637dbec
multiverse22 <momizi527@gmail.com>
2024-09-23 20:55:17 +0900

    fix:변경하려는 이름이 공백인경우에는 예외처리

--- src/main/java/com/sparta/nationofdevelopment/common_entity/ErrorStatus.java
+++ src/main/java/com/sparta/nationofdevelopment/common_entity/ErrorStatus.java
@@ -28,6 +28,7 @@ public enum ErrorStatus implements BaseCode{
     _INVALID_PASSWORD_FORM(HttpStatus.BAD_REQUEST,400,"비밀번호는 최소 8자 이상이어야 하며, 대소문자 포함 영문, 숫자, 특수문자를 최소 1글자씩 포함해야 합니다."),
     _INVALID_USER_INFO(HttpStatus.BAD_REQUEST,400,"변경하려는 정보가 잘못되었습니다."),
     _INVALID_BIRTHDAY(HttpStatus.BAD_REQUEST,400,"잘못된 생일 값입니다"),
+    _PASSWORD_IS_DUPLICATED(HttpStatus.BAD_REQUEST,400,"이미 사용중인 비밀번호로 변경할 수 없습니다."),
 
 
     // 메뉴 예외
